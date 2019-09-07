bff5028ead0cc4e4c1770844df493fa04667e942
Zainul Franciscus <Zainul.Franciscus@myob.com>
2019-09-07 10:40:54 +1000

    initial solution to class and object exercise

--- src/main/scala/ObjectAndClass.scala
+++ src/main/scala/ObjectAndClass.scala
@@ -4,12 +4,25 @@ package classandobject
 class Person(val firstName: String, val lastName: String) {}
 
 object Person {
-  def apply(name: String): Person = ???
+  def apply(name: String): Person = {
+    val names: Array[String] = name.split(" ")
+    names match {
+      case names: Array[String] if names.size == 2 =>
+        new Person(names(0), names(1))
+      case _ =>
+        throw new Exception(
+          s"names can only have 2 names, but $names has more than 2 names")
+    }
+
+  }
 }
+
 
 // * Pattern Matching
 case class Cat(color: String, food: String)
 
 object ChipShop {
-  def willServe(c: Cat): Boolean = ???
+  def willServe(c: Cat): Boolean = {
+    c.food == "Chips"
+  }
 }
